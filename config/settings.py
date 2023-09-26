c2f23382cb6cc2ee006d1c2b55f7a344418dbea6
Javohir <javohirpython@gmail.com>
2023-09-26 18:23:46 +0500

    added statif

--- config/settings.py
+++ config/settings.py
@@ -129,8 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles_build' / "static"
-# STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')))
+STATIC_ROOT = (
+    BASE_DIR / 'static'
+)
+# STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
